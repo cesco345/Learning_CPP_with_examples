ccdc6e2a8b9e6c2c103628a857b0397c363aa729
Francesco Piscani <stem.apks@gmail.com>
2018-03-10 15:48:52 -0500

    Add files via upload

---
+++ chapter0/01_system_pause_linux.cpp
@@ -0,0 +1,15 @@
+
+/*
+ * @topic 03-09-18 system pause in Linux
+ * @brief system pause equivalent
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Press enter to continue ...";
+    std::cin.get();
+    
+    return 0;
+}  
